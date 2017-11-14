b16069cf28ea8a9c0d2e46e000209c1db3a6e0c7
苏红涛 <hongtao.su@corp.elong.com>
2017-11-14 15:45:25 +0800

    db数据源注解支持参数方式

--- src/main/java/com/elong/nb/db/DataSource.java
+++ src/main/java/com/elong/nb/db/DataSource.java
@@ -5,7 +5,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target({ ElementType.METHOD, ElementType.TYPE })
+@Target({ ElementType.PARAMETER, ElementType.METHOD, ElementType.TYPE })
 @Retention(RetentionPolicy.RUNTIME)
 public @interface DataSource {
 
--- src/main/java/com/elong/nb/db/GetDataSourceKeyAspect.java
+++ src/main/java/com/elong/nb/db/GetDataSourceKeyAspect.java
@@ -1,5 +1,6 @@
 package com.elong.nb.db;
 
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
 
 import org.apache.commons.lang.StringUtils;
@@ -14,8 +15,7 @@ public class GetDataSourceKeyAspect {
 		// 拦截的方法名称
 		String methodName = point.getSignature().getName();
 		// 拦截的放参数类型
-		Class<?>[] parameterTypes = ((MethodSignature) point.getSignature())
-				.getMethod().getParameterTypes();
+		Class<?>[] parameterTypes = ((MethodSignature) point.getSignature()).getMethod().getParameterTypes();
 		Class<?>[] clazzs = target.getClass().getInterfaces();
 		Class<?> clazz = null;
 		if (clazzs != null && clazzs.length > 0) {
@@ -25,6 +25,28 @@ public class GetDataSourceKeyAspect {
 		if (clazz == null) {
 			// 这个应该不可能出现
 			throw new RuntimeException("not found Class.");
+		}
+
+		// 拿方法中参数级别的注解，如果参数中有数据源，则固定为方法第一个参数为数据源
+		Annotation[][] annotationss = ((MethodSignature) point.getSignature()).getMethod().getParameterAnnotations();
+		if (annotationss != null && annotationss.length > 0) {
+			boolean isExistParamDataSource = false;
+			for (Annotation[] annotations : annotationss) {
+				if (annotations == null || annotations.length == 0)
+					continue;
+				for (Annotation annotation : annotations) {
+					if (annotation.annotationType().equals(DataSource.class)) {
+						isExistParamDataSource = true;
+						break;
+					}
+				}
+			}
+			if (isExistParamDataSource) {
+				Object[] args = point.getArgs();
+				String dataSource = (String) args[0];
+				DataSourceKeyHolder.setDataSourceKey(dataSource);
+				return;
+			}
 		}
 
 		// 拿接口中的方法级别的注解
